4b8aa49d6d2be94384aed17e103abe692cb7ddd1
Niklas Ekman <niklas@stockfiller.com>
2021-11-18 11:41:52 +0100

    Fix permissions

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 gosu "$USER" "$STEAMCMDDIR/steamcmd.sh" +login anonymous +force_install_dir "$GAME_DIR" +app_update "$GAME_ID" validate +quit
 
-gosu "$USER" /start-server.sh
+gosu "$USER" bash /start-server.sh
