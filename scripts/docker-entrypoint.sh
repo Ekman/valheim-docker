6a67d8bd7061375f884022992005f01ac79770a9
Niklas Ekman <niklas@stockfiller.com>
2021-11-18 11:19:36 +0100

    Start server using exec

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 su "$USER" -c "$STEAMCMDDIR/steamcmd.sh +login anonymous +force_install_dir $GAME_DIR +app_update $GAME_ID validate +quit"
 
-su "$USER" /start-server.sh
+exec su "$USER" /start-server.sh
