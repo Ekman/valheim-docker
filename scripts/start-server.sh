48ee3580858414541a65e4fa044f6f218f616264
Niklas Ekman <niklas@stockfiller.com>
2021-11-21 19:50:18 +0100

    Revert "Set SteamAppId=GAME_ID"
    This reverts commit c7e81ed804210abcbe94ad4a5e24fd472cd352d8.

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
-export SteamAppId=$GAME_ID
+export SteamAppId=892970
 
 echo "Starting server PRESS CTRL-C to exit"
 
