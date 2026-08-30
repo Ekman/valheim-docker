46a5134a63ca582b5c0128008457c3242993aaea
Niklas Ekman <niklas.ekman1@proton.me>
2026-08-30 20:45:31 +0200

    Simplify start-server.sh by removing LD_LIBRARY_PATH export
    Removed unnecessary export of LD_LIBRARY_PATH.

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -8,12 +8,10 @@ echo "Starting server PRESS CTRL-C to exit"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-"$GAME_DIR/valheim_server.x86_64" \
+exec "$GAME_DIR/valheim_server.x86_64" \
     -name "$NAME" \
     -world "$WORLD" \
     -port "$PORT" \
     -password "$PASSWORD" \
     -public "$PUBLIC" \
     -savedir "$CONFIG_DIR"
-
-export LD_LIBRARY_PATH=$templdpath
